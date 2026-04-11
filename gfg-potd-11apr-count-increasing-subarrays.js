fd9195c31c6e6a05db5c0fda7500041e40e41de8
Sorabh Kumar Singh <sourabhsingh01998@gmail.com>
2026-04-11 12:17:40 +0530

    Add function to count increasing subarrays
    Implement a function to count strictly increasing subarrays of size >= 2.

---
+++ gfg-potd-11apr-count-increasing-subarrays.js
@@ -0,0 +1,43 @@
+/*
+Given an array arr[] of integers, count the number of subarrays in arr[] which are strictly increasing with size greater or equal to 2. A subarray is a contiguous part of array. A subarray is strictly increasing if each element is greater then it's previous element if it exists.
+
+Examples:
+
+Input: arr[] = [1, 4, 5, 3, 7, 9]
+Output: 6
+Explanation: The strictly increasing subarrays are: [1, 4], [1, 4, 5], [4, 5], [3, 7], [3, 7, 9], [7, 9]
+Input: arr[] = [1, 3, 3, 2, 3, 5]
+Output: 4
+Explanation: Increasing Subarrays of size greater or equal to 2 are : {1, 3}, {2, 3}, {2, 3, 5}, {3, 5}. So answer for this test case is 4.
+Input: arr[] = [2, 2, 2, 2]
+Output: 0
+Explanation: No strictly increasing subarray exists.
+Constraints:
+1 ≤ arr.size() ≤ 105
+1 ≤ arr[i] ≤ 107
+*/
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+class Solution {
+    countIncreasing(arr) {
+        // code here.
+        let n = arr.length;
+        let count = 0;
+        let length = 1;
+        
+        for(let i = 0; i < n; i++){
+            if(arr[i] > arr[i - 1]){
+                length++;
+            }else{
+                count += (length * (length - 1)) / 2; 
+                length = 1;
+            }
+        }
+        
+        count += (length * (length - 1)) / 2;
+        
+        return count;
+    }
+}
